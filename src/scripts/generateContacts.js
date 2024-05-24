bae50e0147a01f6c4057311e51bf628a66a5b721
Dmytro Maniuta <ip722428@gmail.com>
2024-05-24 18:51:03 +0300

    started hw-1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+let data = [];
 
+const generateContacts = async (number) => {
+  for (let i = 1; i <= number; i++) {
+    data.push(createFakeContact());
+  }
+};
+// console.log(createFakeContact);
 await generateContacts(5);
+
+(async () => {
+  // const data = 'Це дані, які ми записуємо у файл.';
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(data), 'utf8');
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+})();
