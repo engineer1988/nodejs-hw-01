06047c9ec23f1c557307079e6c64b851204e7b8b
Dmytro Maniuta <ip722428@gmail.com>
2024-05-25 16:04:08 +0300

    finished step 5

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    let contacts = JSON.parse(await fs.readFile(PATH_DB));
+    console.log('Дані успішно виведені в консоль для читання.');
+    return 'Кількість контактів: ' + contacts.length;
+  } catch (err) {
+    console.error('Помилка виведеня в консоль:', err);
+  }
+};
 
 console.log(await countContacts());
