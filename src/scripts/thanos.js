408ffeda351b481242c1ed33de88a40236af11e0
Dmytro Maniuta <ip722428@gmail.com>
2024-05-25 20:40:35 +0300

    finished step 7

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    let contacts = JSON.parse(await fs.readFile(PATH_DB));
+    let newContacts = [];
+    contacts.forEach(function (contact) {
+      if (Math.random() >= 0.5) {
+        newContacts.push(contact);
+      }
+    });
+    await fs.writeFile(PATH_DB, JSON.stringify(newContacts), 'utf8');
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+};
 
 await thanos();
